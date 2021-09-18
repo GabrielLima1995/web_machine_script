b2b893c22c5363fd64daa3ca3667d8297ef6e50c
GabrielLima1995 <gabriel-souzalima@live.com>
2021-09-17 21:19:29 -0300

    Add sphinx commit

--- initialize.bash
+++ initialize.bash
@@ -1,1 +1,7 @@
 sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install apache
+sudo apt install firewalld
+sudo apt install nodejs
+sudo apt install npm
+sudo npm install pm2 -g
---
+++ sphinx.bash
@@ -0,0 +1,9 @@
+sudo apt-get install pandoc 
+sudo apt-get install python3-sphinx
+sudo -H pip install sphinx-autobuild
+sudo -H pip install jupyter-sphinx
+sudo -H pip install nbsphinx
+sudo -H pip install sphinx-rtd-theme
+sudo -H pip install pip install sphinx-rtd-dark-mode
+sudo -H pip install sphinx-math-dollar
+
